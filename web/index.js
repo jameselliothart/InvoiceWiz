9911f74986d9a7e264163ccc2c75ac6a4e8b9e68
James Hart <james.elliot.hart@gmail.com>
2025-02-27 20:48:18 -0500

    submit to post to MT

--- web/index.js
+++ web/index.js
@@ -1,6 +1,39 @@
-function handleSubmit(event) {
-    event.preventDefault();
-    const amount = event.target.invoiceAmount.value;
-    // fetch
-    alert(amount);
+const URI_INVOICE = 'http://localhost:8080/api/invoice';
+
+async function handleSubmit(event) {
+  event.preventDefault();
+  const msg = {
+    id: crypto.randomUUID(),
+    to: event.target.invoiceTo.value,
+    amount: Number(event.target.invoiceAmount.value),
+  };
+  try {
+    const response = await fetch(URI_INVOICE, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(msg),
+    });
+    if (!response.ok) {
+      throw new Error(`Response status: ${response.status}`);
+    }
+  } catch (error) {
+    console.error(error.message);
+  }
+}
+
+async function getInvoices() {
+  try {
+    const response = await fetch(URI_INVOICE);
+    if (!response.ok) {
+      throw new Error(`Response status: ${response.status}`);
+    }
+
+    const json = await response.json();
+    console.log(json);
+    return json;
+  } catch (error) {
+    console.error(error.message);
+  }
 }
