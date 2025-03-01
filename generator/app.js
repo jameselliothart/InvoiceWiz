fb7936bb65a9e0a1b8e9d6dfa4394094132b18b6
James Hart <james.elliot.hart@gmail.com>
2025-02-28 20:27:00 -0500

    poc s3 integration with localstack

---
+++ generator/app.js
@@ -0,0 +1,102 @@
+const {
+  S3Client,
+  CreateBucketCommand,
+  GetObjectCommand,
+  HeadBucketCommand,
+  waitUntilBucketExists,
+  ListObjectsV2Command,
+  PutObjectCommand
+} = require('@aws-sdk/client-s3');
+const { fromEnv } = require('@aws-sdk/credential-providers');
+const { getSignedUrl } = require('@aws-sdk/s3-request-presigner');
+const fs = require('fs');
+const path = require('path');
+
+async function generateSignedUrl(s3Client, bucket, key) {
+  try {
+    const getObjectCommand = new GetObjectCommand({ Bucket: bucket, Key: key });
+    const getObjectUrl = await getSignedUrl(
+      s3Client, getObjectCommand, { expiresIn: 3600, responseContentDisposition: 'inline' }
+    );
+    return getObjectUrl;
+  } catch (error) {
+    console.error(`Error generating signed URL for ${key}:`, error);
+    return null;
+  }
+}
+
+async function uploadFile(s3Client, filePath, bucket) {
+  const fileStream = fs.createReadStream(filePath);
+  fileStream.on('error', err => console.error('File error', err));
+  const fileName = path.basename(filePath);
+
+  const uploadParams = {
+    Bucket: bucket,
+    Key: fileName,
+    Body: fileStream,
+  };
+
+  const putObjectCommand = new PutObjectCommand(uploadParams);
+  await s3Client.send(putObjectCommand);
+  console.log(`Uploaded ${fileName} to ${bucket}`);
+}
+
+async function createBucketIdempotent(s3Client, bucket) {
+  try {
+    const headBucketCommand = new HeadBucketCommand({ Bucket: bucket });
+    await s3Client.send(headBucketCommand);
+    console.log(`Bucket ${bucket} already exists`);
+  } catch (err) {
+    if (err.name === 'NotFound') {
+      const createBucketCommand = new CreateBucketCommand({ Bucket: bucket });
+      await s3Client.send(createBucketCommand);
+      console.log(`Bucket ${bucket} successfully created`);
+    } else {
+      throw err;
+    }
+  }
+}
+
+async function main() {
+
+  process.env.AWS_ACCESS_KEY_ID = 'test';
+  process.env.AWS_SECRET_ACCESS_KEY = 'test';
+
+  const s3Client = new S3Client({
+    region: 'us-east-1',
+    endpoint: 'http://localhost:4566',
+    forcePathStyle: true,
+    credentials: fromEnv(),
+  });
+
+  const bucket = 'invoice-files'
+
+  try {
+    await createBucketIdempotent(s3Client, bucket);
+
+    await waitUntilBucketExists({ client: s3Client, maxWaitTime: 20 }, { Bucket: bucket });
+    console.log(`Bucket ${bucket} is ready`);
+
+    await uploadFile(s3Client, 'test.txt', bucket);
+
+    const listObjectsCommand = new ListObjectsV2Command({ Bucket: bucket });
+    const data = await s3Client.send(listObjectsCommand);
+
+    if (!data.Contents || data.Contents.length === 0) {
+      console.log('No objects found in the bucket');
+      return;
+    }
+
+    const imageKeys = data.Contents.map((object) => object.Key);
+    console.log('Found keys:', imageKeys);
+
+    const signedUrls = await Promise.all(imageKeys.map((key) => generateSignedUrl(s3Client, bucket, key)));
+    console.log('Signed URLs for the uploaded files:', signedUrls);
+
+    return signedUrls;
+  } catch (err) {
+    console.error(`Failed to complete operations for bucket ${bucket}:`, err);
+  }
+}
+
+main();
