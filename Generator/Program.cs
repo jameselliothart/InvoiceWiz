4279db8215c57dd509e07604622b9aa7871daf15
James Hart <james.elliot.hart@gmail.com>
2025-03-21 18:12:35 -0400

    configure app

--- Generator/Program.cs
+++ Generator/Program.cs
@@ -1,5 +1,21 @@
+using Generator.Invoices;
+using Generator.Storage;
+using MassTransit;
 
 var builder = Host.CreateApplicationBuilder(args);
+
+builder.Services.Configure<AzureBlobStorageOptions>(builder.Configuration.GetSection("AzureBlobStorage"));
+builder.Services.AddSingleton<IBlobStorageService, AzureBlobStorageService>();
+builder.Services.AddMassTransit(c =>
+{
+    c.AddConsumer<InvoiceRequestedConsumer>();
+    c.UsingRabbitMq((ctx, cfg) =>
+    {
+        cfg.Host("rabbitmq://broker");
+        cfg.ConfigureEndpoints(ctx);
+        cfg.UseMessageRetry(r => r.Interval(5, TimeSpan.FromSeconds(5)));
+    });
+});
 
 var host = builder.Build();
 host.Run();
