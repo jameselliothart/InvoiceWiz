cce5a6412a900a8ab9ef844a2f50bfe25a105ed6
James Hart <james.elliot.hart@gmail.com>
2025-03-21 14:23:49 -0400

    register mongo and mass transit

--- Persister/Program.cs
+++ Persister/Program.cs
@@ -1,7 +1,28 @@
-using Persister;
+using MassTransit;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization;
+using MongoDB.Bson.Serialization.Serializers;
+using MongoDB.Driver;
+using Persister.Invoices;
 
 var builder = Host.CreateApplicationBuilder(args);
-builder.Services.AddHostedService<Worker>();
+// Register MongoDB
+builder.Services.AddSingleton<IMongoClient>(sp => new MongoClient("mongodb://mongodb:27017"));
+builder.Services.AddSingleton(sp => sp.GetRequiredService<IMongoClient>().GetDatabase("InvoiceDb"));
+
+builder.Services.AddMassTransit(c =>
+{
+    c.AddConsumer<InvoiceRequestedConsumer>();
+    c.AddConsumer<InvoiceGeneratedConsumer>();
+    c.UsingRabbitMq((ctx, cfg) =>
+    {
+        cfg.Host("rabbitmq://broker");
+        cfg.ConfigureEndpoints(ctx);
+        cfg.UseMessageRetry(r => r.Interval(5, TimeSpan.FromSeconds(5)));
+    });
+});
+
+BsonSerializer.RegisterSerializer(new GuidSerializer(GuidRepresentation.Standard));
 
 var host = builder.Build();
 host.Run();
