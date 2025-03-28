0889657b18e0a55d6e6ffe28c08da985606db470
James Hart <james.elliot.hart@gmail.com>
2025-03-27 21:02:35 -0400

    implement grpc invoice service

--- Search/Services/InvoiceService.cs
+++ Search/Services/InvoiceService.cs
@@ -1,3 +1,4 @@
+using Contracts;
 using Grpc.Core;
 using Search.DataAccess;
 using Search.Grpc;
@@ -6,30 +7,81 @@ namespace Search.Services;
 
 public class InvoiceService(IInvoiceRepository _repo, ILogger<InvoiceService> _logger) : InvoiceSearchService.InvoiceSearchServiceBase
 {
-    public override async Task<GetInvoiceUrlReply> GetInvoiceUrl(GetInvoiceUrlRequest request, ServerCallContext context)
+    public override async Task<GetInvoicesReply> GetInvoices(GetInvoicesRequest request, ServerCallContext context)
     {
-        using (_logger.BeginScope(new Dictionary<string, object>{["invoiceId"] = request.Id}))
+        _logger.LogInformation("Retrieving all invoices");
+        var invoices = await _repo.GetAllAsync();
+        _logger.LogInformation("Retrieved {invoiceCount} invoices", invoices.Count);
+        _logger.LogInformation("Preparing reply");
+        var invoiceOverviews = invoices.Select(SerializeToProto).Where(i => i != null).ToList();
+        _logger.LogInformation("Reply has {protoMsgCount} invoices", invoiceOverviews.Count);
+        if (invoices.Count != invoiceOverviews.Count)
+            _logger.LogError(
+                "There were errors serializing some invoices: {invoiceCount} vs {protoMsgCount}. Check logs.",
+                invoices.Count,
+                invoiceOverviews.Count
+            );
+        var reply = new GetInvoicesReply();
+        reply.Invoices.AddRange(invoiceOverviews);
+        _logger.LogInformation("Prepared reply");
+        return reply;
+    }
+
+    public InvoiceOverview? SerializeToProto(Invoice invoice)
+    {
+        using (_logger.BeginScope(new Dictionary<string, object> { ["invoiceId"] = invoice.Id }))
         {
+            try
+            {
+                _logger.LogInformation("Serializing");
+                var id = invoice.Id.ToString();
+                var url = invoice.Location.ToString();
+                var to = invoice.To;
+                var amount = invoice.Amount.ToString("G29");
+                var invoiceDate = invoice.InvoiceDate.ToString("yyyy-MM-dd");
+                var createdDate = invoice.CreatedDate.ToString("o");
+                var overview = new InvoiceOverview
+                    {
+                        Id = id,
+                        Url = url,
+                        To = to,
+                        Amount = amount,
+                        InvoiceDate = invoiceDate,
+                        CreatedDate = createdDate,
+                    };
+                _logger.LogInformation("Serialized");
+                return overview;
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "Serialization failed for {invoice}", invoice);
+                return null;
+            }
+        }
+    }
+
+    public override async Task<GetInvoiceReply> GetInvoice(GetInvoiceRequest request, ServerCallContext context)
+    {
+        using (_logger.BeginScope(new Dictionary<string, object> { ["invoiceId"] = request.Id }))
+        {
+            // TODO codify errors as oneof message replies
             if (!Guid.TryParse(request.Id, out var invoiceId))
             {
                 _logger.LogError("Invalid invoice ID format");
                 throw new RpcException(new Status(StatusCode.InvalidArgument, "Invalid invoice ID format"));
             }
+            _logger.LogInformation("Retrieving");
             var invoice = await _repo.GetByIdAsync(invoiceId);
             if (invoice == null)
             {
                 _logger.LogWarning("NotFound");
                 throw new RpcException(new Status(StatusCode.NotFound, $"Cannot find id {invoiceId}"));
             }
-            if (string.IsNullOrEmpty(invoice.Location.ToString()))
+            _logger.LogInformation("Retrieved");
+            return new GetInvoiceReply
             {
-                _logger.LogError("Missing invoice url");
-                throw new RpcException(new Status(StatusCode.NotFound, $"Missing invoice url for {invoiceId}"));
-            }
-            return new GetInvoiceUrlReply
-            {
-                Id = request.Id,
-                Url = invoice.Location.ToString(),
+                Overview = SerializeToProto(invoice),
+                Details = invoice.Details ?? "",
             };
         }
     }
