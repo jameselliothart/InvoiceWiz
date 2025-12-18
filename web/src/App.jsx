a6aded097a2641ddfff45f4ebf1357bb585b826f
James Hart <james.elliot.hart@gmail.com>
2025-12-18 13:12:57 -0500

    fix up styling and remove react logo

--- web/src/App.jsx
+++ web/src/App.jsx
@@ -1,4 +1,3 @@
-import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import InvoiceForm from './features/invoiceForm/InvoiceForm'
@@ -7,19 +6,20 @@ import InvoiceGrid from './features/invoiceGrid/InvoiceGrid'
 function App() {
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="container mt-4">
+      <div className="row justify-content-center">
+        <div className="col-12 text-center mb-3">
+          <a href="https://vite.dev" target="_blank" rel="noopener noreferrer">
+            <img src={viteLogo} className="logo" alt="Vite logo" />
+          </a>
+        </div>
+        <div className="col-md-8">
+          <h1 className="text-center">Welcome to Invoice Wizard</h1>
+          <InvoiceForm />
+          <InvoiceGrid />
+        </div>
       </div>
-      <h1>Welcome to Invoice Wizard</h1>
-      <InvoiceForm />
-      <InvoiceGrid />
-    </>
+    </div>
   )
 }
 
